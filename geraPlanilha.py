cdddef45bf265df447d70bfabea0faeed214f9d4
MakalisterAndrade <makalister.andrade@outlook.com>
2023-11-21 22:22:44 -0400

    validação correta para a coluna Resumo

--- geraPlanilha.py
+++ geraPlanilha.py
@@ -29,7 +29,14 @@ def process_file(filename):
                 while value_index < len(row) and (not row[value_index] or ":" in str(row[value_index])):
                     value_index += 1
                 value = row[value_index] if value_index < len(row) else None
-                item[current_title] = value
+
+                if current_title == "Resumo":
+                    # Se o título for "Resumo" e a célula à direita contiver dados, adiciona a célula à direita como valor/descrição do resumo
+                    description = str(row[i + 1]) if i + 1 < len(row) and row[i + 1] else ""
+                    item[current_title] = description
+                elif value is not None:
+                    item[current_title] = value
+
                 current_title = None
 
     if item:
